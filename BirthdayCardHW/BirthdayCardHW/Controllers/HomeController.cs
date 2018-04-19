1182823bb37467108f1fde82f872ed294f53f93d
Richard Dalton <rick@rdaltonphotos.com>
2018-04-19 01:54:51 -0700

    Initial Submit of Homework 1

---
+++ BirthdayCardHW/BirthdayCardHW/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardHW.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(Models.BirthdayCardModel bdayCardResponse )
+    {
+      if (ModelState.IsValid)
+      {
+        return View("BirthdayCard", bdayCardResponse);
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+  }
+}
---
+++ BirthdayCardHW/BirthdayCardHW/Models/BirthdayCardModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayCardHW.Models
+{
+  public class BirthdayCardModel
+  {
+    [Required(ErrorMessage = "Please enter who the wish is from")]
+    public string From { get; set; }
+    [Required(ErrorMessage = "Please enter who the wish is to")]
+    public string To { get; set; }
+    [Required(ErrorMessage = "Please enter the message for the birtday wish")]
+    public string Message { get; set; }
+  }
+}
