5aa19f863a18e0c3a02c831d62a44e5b2726ea6d
Richard Dalton <rick@rdaltonphotos.com>
2018-04-26 20:52:46 -0700

    Initial commit for HW 2

---
+++ HomePageHW2/HomePageHW2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomePageHW2.Startup))]
+namespace HomePageHW2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
